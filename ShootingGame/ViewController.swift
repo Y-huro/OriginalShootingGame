d9820d7cea5fc2388e2d2af7898b4091cee580d1
y.i <miko220284yi@gmail.com>
2022-02-13 15:02:27 +0900

    Initial Commit

---
+++ ShootingGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShootingGame
+//
+//  Created by y i on 2022/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
